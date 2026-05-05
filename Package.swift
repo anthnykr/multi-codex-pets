cff313ddcacd26b2eea8118b51a36a134b358b3d
unityeagleai <71unityeagle@gmail.com>
2026-05-05 17:29:54 +0200

    Add macOS 13 compatibility (#1)

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
   name: "MultiCodexPets",
   platforms: [
-    .macOS(.v14)
+    .macOS(.v13)
   ],
   products: [
     .executable(name: "MultiCodexPets", targets: ["MultiCodexPets"]),
